b57b816086e733e891273320c1076d635b018be1
Jeremy Green <jeremy@octolabs.com>
2016-04-30 16:08:29 -0500

    Return instructions if the query params are missing

--- left-pad/handler.js
+++ left-pad/handler.js
@@ -2,8 +2,18 @@
 
 let leftpad = require('left-pad');
 
+function instructions(){
+  return {
+    "instructions": "Include at least a 'string' query param, and optinally a 'padding' param.",
+    "example": "?string=Just+a+test&padding=20"
+  };
+}
+
 module.exports.handler = function(event, context, cb) {
-  var string = event.string;
+  if(!event.string && !event.padding){
+    return cb(null, instructions());
+  }
+  var string = event.string || "";
   var padding = event.padding || 0;
   var paddedString = leftpad(string,padding);
   var payload = {
