0952ab0c6fab9fb6d9430ef708895159dc1b3e4b
Jeremy Green <jeremy@octolabs.com>
2016-04-30 15:37:18 -0500

    Require left-pad

--- left-pad/handler.js
+++ left-pad/handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let leftpad = require('left-pad');
+
 module.exports.handler = function(event, context, cb) {
   return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
