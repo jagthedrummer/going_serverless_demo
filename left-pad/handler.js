f26d50ec3fe350b1121ef4be545fc282161c55cf
Jeremy Green <jeremy@octolabs.com>
2016-04-30 15:54:05 -0500

    Implement a real left-pad handler

--- left-pad/handler.js
+++ left-pad/handler.js
@@ -3,7 +3,11 @@
 let leftpad = require('left-pad');
 
 module.exports.handler = function(event, context, cb) {
-  return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
+  var string = event.string;
+  var padding = event.padding || 0;
+  var paddedString = leftpad(string,padding);
+  var payload = {
+    paddedString: paddedString
+  };
+  return cb(null, payload);
 };
