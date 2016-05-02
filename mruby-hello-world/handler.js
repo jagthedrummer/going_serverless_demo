a1f11b2833b85f3fed34e1dc6ecad978041f65a3
Jeremy Green <jeremy@octolabs.com>
2016-05-02 17:25:37 -0500

    Add a bash script and a modified handler.
    Yoinked from qrush : https://github.com/qrush/lambda-function-wrapper-ruby

--- mruby-hello-world/handler.js
+++ mruby-hello-world/handler.js
@@ -1,7 +1,9 @@
 'use strict';
 
-module.exports.handler = function(event, context, cb) {
-  return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
-};
+var spawn = require('child_process').spawn;
+module.exports.handler = function(event, context) {
+    var child = spawn('./lambda-function', [JSON.stringify(event, null, 2)]);
+    child.stdout.on('data', function (data) { console.log("stdout:\n"+data); });
+    child.stderr.on('data', function (data) { console.log("stderr:\n"+data); });
+    child.on('close', function (code) { context.done(); });
+}
