871091bbe7bb9aa1eb46c3a44ecf0813bbea0f3d
Jeremy Green <jeremy@octolabs.com>
2016-05-02 20:24:42 -0500

    Capture ruby output for the API response

--- mruby-hello-world/handler.js
+++ mruby-hello-world/handler.js
@@ -1,9 +1,21 @@
 'use strict';
 
 var spawn = require('child_process').spawn;
-module.exports.handler = function(event, context) {
+module.exports.handler = function(event, context, callback) {
     var child = spawn('./mruby', ['handler.rb', JSON.stringify(event, null, 2)]);
-    child.stdout.on('data', function (data) { console.log("stdout:\n"+data); });
-    child.stderr.on('data', function (data) { console.log("stderr:\n"+data); });
-    child.on('close', function (code) { context.done(); });
+    var rubyOutput = [];
+    child.stdout.on('data',function (data) {
+      rubyOutput.push(data.toString());
+      console.log("stdout:\n"+data);
+    });
+    child.stderr.on('data', function (data) {
+      rubyOutput.push(data);
+      console.log("stderr:\n"+data.toString());
+    });
+    child.on('close', function (code) {
+      callback(null,{
+        message: "We're back from ruby land",
+        rubyOutput: rubyOutput
+      });
+    });
 }
